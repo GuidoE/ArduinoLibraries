373d201b140f84f6fe8f9d42ec37883d401bde5a
Guido Espinosa <guido@superg.co>
2015-08-30 23:24:11 -0500

    uses 30 seconds of calibration instead of 10

--- PIR.cpp
+++ PIR.cpp
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 #include "PIR.h"
 
-PIR::PIR(int pin=7, int led=13, int calibration=10) {
+PIR::PIR(int pin=7, int led=13, int calibration=30) {
 	pirPin = pin;
 	ledPin = led;
 	calibrationTime = calibration;
